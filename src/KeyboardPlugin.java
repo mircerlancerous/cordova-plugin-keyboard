d9b75f65c9f38b8e9412cfa730e441cdfec8a082
OffTheBricks <mircerlancerous@users.noreply.github.com>
2016-08-24 22:53:23 -0700

    Create KeyboardPlugin.java

---
+++ src/KeyboardPlugin.java
@@ -0,0 +1,77 @@
+package com.otb.cordova.keyboard;
+/*
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+
+import com.acs.smartcard.Reader;
+import com.acs.smartcard.ReaderException;
+*/
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+/*
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+*/
+public class KeyboardPlugin extends CordovaPlugin  {
+    private UsbManager usbManager;
+    private UsbDevice usbDevice;
+
+    private Reader reader;
+    private PendingIntent mPermissionIntent;
+    
+    private CallbackContext keyup_callback = null;
+    private CallbackContext keydown_callback = null;
+    /*
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        
+    }
+	*/
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        
+        if (action.equalsIgnoreCase("keyUp")) {
+            keyup_callback = callbackContext;
+        }
+        else if(action.equalsIgnoreCase("keyDown")){
+            keydown_callback = callbackContext;
+        }
+        else {
+            // invalid action
+            return false;
+        }
+        
+        PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        result.setKeepCallback(true);
+        callbackContext.sendPluginResult(result);
+    //    callback = callbackContext;
+
+        return true;
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        PluginResult result = new PluginResult(PluginResult.Status.OK, Integer.toString(keyCode));
+        result.setKeepCallback(true);
+        keydown_callback.sendPluginResult(result);
+    }
+    
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event){
+        PluginResult result = new PluginResult(PluginResult.Status.OK, Integer.toString(keyCode));
+        result.setKeepCallback(true);
+        keyup_callback.sendPluginResult(result);
+    }
+}
