796bc59da9c25c49dc24ea757304ec8b0bea5f02
OffTheBricks <mircerlancerous@users.noreply.github.com>
2016-08-25 20:06:17 -0700

    Update KeyboardPlugin.java

--- src/android/KeyboardPlugin.java
+++ src/android/KeyboardPlugin.java
@@ -1,18 +1,5 @@
 package com.otb.cordova.keyboard;
-/*
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-import android.util.Log;
 
-import com.acs.smartcard.Reader;
-import com.acs.smartcard.ReaderException;
-*/
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -26,21 +13,9 @@ import org.json.JSONObject;
 import android.view.KeyEvent;
 
 public class KeyboardPlugin extends CordovaPlugin  {
-    /*private UsbManager usbManager;
-    private UsbDevice usbDevice;
-
-    private Reader reader;
-    private PendingIntent mPermissionIntent;
-    */
     private CallbackContext keyup_callback = null;
     private CallbackContext keydown_callback = null;
-    /*
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        
-    }
-	*/
+    
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         
@@ -64,12 +39,23 @@ public class KeyboardPlugin extends CordovaPlugin  {
         PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
         result.setKeepCallback(true);
         callbackContext.sendPluginResult(result);
-    //    callback = callbackContext;
 
         return true;
     }
     
-    //@Override
+    OnKeyListener keyListener = new OnKeyListener() {
+	    @Override
+	    public boolean onKey(View v, int keyCode, KeyEvent event) {
+	        if (event.getAction() == KeyEvent.ACTION_UP) {
+	            onKeyUp(keyCode, event);
+	        }
+	        else if (event.getAction() == KeyEvent.ACTION_DOWN) {
+	        	onKeyDown(keyCode, event);
+	        }
+	        return false;
+	    }
+	};
+    
     public boolean onKeyDown(int keyCode, KeyEvent event){
     	if(keydown_callback == null){
     		return true;
@@ -80,7 +66,6 @@ public class KeyboardPlugin extends CordovaPlugin  {
         return false;
     }
     
-    //@Override
     public boolean onKeyUp(int keyCode, KeyEvent event){
     	if(keyup_callback == null){
     		return true;
